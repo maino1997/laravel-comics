67c07116c0a7b1905b8a9b75cc63bd6c2941fdb5
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-15 16:24:06 +0100

    first commit

---
+++ resources/views/layout/home.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header-top"></div>
+            <div class="header-bottom"></div>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-5">
+                    <div class="col-3">
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3">
+                        <ul>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                            <li><a href="#"></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+                <div class="col-5"></div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
