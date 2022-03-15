190cb2e0d476b60fa1d867a174bd37604dc1d490
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-15 16:50:16 +0100

    error in extends

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,9 +10,22 @@
 
 <body>
     <header>
-        <div class="container">
-            <div class="header-top"></div>
-            <div class="header-bottom"></div>
+        <div>
+            <img src="{{ asset('images/dc-logo.png') }}" />
+        </div>
+        <div class="list-cont">
+            <ul>
+                <li><a href="#">CHARACTERS</a></li>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLETABLES</a></li>
+                <li><a href="#">VIDEOS</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
         </div>
     </header>
     <main>
