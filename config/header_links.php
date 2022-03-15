c72ffdd6ac189add84e39ee3ff6adc016e71f826
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-15 19:25:45 +0100

    header datas and link in config file

---
+++ config/header_links.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'text' => 'CHARACTERS',
+        'link' => '#'
+    ],
+    [
+        'text' => 'COMICS',
+        'link' => '#'
+    ],
+    [
+        'text' => 'MOVIES',
+        'link' => '#'
+    ],
+    [
+        'text' => 'TV',
+        'link' => '#'
+    ],
+    [
+        'text' => 'GAMES',
+        'link' => '#'
+    ],
+    [
+        'text' => 'COLLECTABLES',
+        'link' => '#'
+    ],
+    [
+        'text' => 'VIDEOS',
+        'link' => '#'
+    ],
+    [
+        'text' => 'FANS',
+        'link' => '#'
+    ],
+    [
+        'text' => 'NEWS',
+        'link' => '#'
+    ],
+    [
+        'text' => 'SHOP',
+        'link' => '#'
+    ]
+];
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,19 +1,19 @@
+@php
+
+$links = config('header_links');
+
+@endphp
+
+
 <header>
     <div>
         <img src="{{ asset('images/dc-logo.png') }}" />
     </div>
     <div class="list-cont">
         <ul>
-            <li><a href="#">CHARACTERS</a></li>
-            <li><a href="#">COMICS</a></li>
-            <li><a href="#">MOVIES</a></li>
-            <li><a href="#">TV</a></li>
-            <li><a href="#">GAMES</a></li>
-            <li><a href="#">COLLETABLES</a></li>
-            <li><a href="#">VIDEOS</a></li>
-            <li><a href="#">FANS</a></li>
-            <li><a href="#">NEWS</a></li>
-            <li><a href="#">SHOP</a></li>
+            @foreach ($links as $link)
+                <li><a href="{{ $link['link'] }}">{{ $link['text'] }}</a></li>
+            @endforeach
         </ul>
     </div>
 </header>
