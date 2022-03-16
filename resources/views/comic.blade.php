c7503e53bfea56b539fc4bdd820c5e03d3d63429
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-16 17:13:37 +0100

    upper page style

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,7 +1,23 @@
 @extends('layout.main')
 
 @section('content')
-    <h2>
-        {{ $comic['title'] }}
-    </h2>
+    <section id="comic">
+        <div class="separator-bar"></div>
+        <div class="container">
+            <div class="col-8">
+                <h2>{{ $comic['title'] }}</h2>
+                <div class="green-bar">
+                    <span><span class="light">Price:</span>{{ $comic['price'] }} </span>
+                    <div class="right">
+                        <span class="light">AVAILABLE</span>
+                        <span class="border">Check Availability</span>
+                    </div>
+                </div>
+                <p>{{ $comic['description'] }}</p>
+            </div>
+            <div class="col-4">
+                <img src="../images/adv.jpg" alt="adv">
+            </div>
+        </div>
+    </section>
 @endsection
